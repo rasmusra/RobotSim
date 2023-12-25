1beeaefca4311a7f19751daf4e2e6828c47be028
Rasmus Rasmussen <yacrumb@yahoo.se>
2023-12-25 09:09:15 +0100

    Add Surface class

---
+++ RobotSim.UnitTests/SurfaceTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace RobotSim.UnitTests;
+
+public class SurfaceTests
+{
+    [Fact]
+    public void WhenCreated_GivenDimension_ThenInBoundsIsAvailable()
+    {
+        const uint Width = 5;
+        const uint Height = 5;
+        var surface = new Surface(Width, Height);
+
+        Assert.Equal(Width, surface.Width);
+        Assert.Equal(Height, surface.Height);
+    }
+}
---
+++ RobotSim/Surface.cs
@@ -0,0 +1,14 @@
+﻿namespace RobotSim
+{
+    public class Surface
+    {
+        public Surface(uint width, uint height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public uint Width { get; }
+        public uint Height { get; }
+    }
+}
